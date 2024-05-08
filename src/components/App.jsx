336d8c189b97f92c8050bb9f203f03a31ce956da
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-05-08 21:35:57 +0200

    6tasthopedone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
 import ContactForm from "./ContactForm/ContactForm.jsx";
 import SearchBox from "./SearchBox/SearchBox.jsx";
 import ContactList from "./ContactList/ContactList.jsx";
-import { useSelector } from "react-redux";
 
 export default function App() {
-  const selectContacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.name);
-  const foundContact = selectContacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <div>
       <h1>Phonebook</h1>
 
       <ContactForm />
       <SearchBox />
-      <ContactList contacts={foundContact} />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,17 @@
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
 
-export default function ContactList({ contacts }) {
+export default function ContactList() {
+  const selectContacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+  const foundContact = selectContacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <>
       <ul className={css.list}>
-        {contacts.map((contact) => (
+        {foundContact.map((contact) => (
           <li key={contact.id}>
             <Contact contact={contact} />
           </li>
